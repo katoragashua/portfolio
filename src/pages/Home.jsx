4d55921d1486fb2c1033a57d9c23631bdf304f2a
katoragashua <katoragashua@outlook.com>
2023-01-13 23:23:28 +0100

    UI changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const TypewriterComponent = () => {
   return (
     <Typewriter
       options={{
-        strings: ["Kator Agashua"],
+        strings: ["KATOR AGASHUA"],
         autoStart: true,
         loop: true,
       }}
@@ -20,45 +20,22 @@ const TypewriterComponent = () => {
 const Home = (props) => {
   const { lang, translations, count } = useTranslation();
   const current = Object.keys(translations).find((key) => key === lang[count]);
-  const nameRef = useRef(null);
-
-  const [name, setName] = useState("");
-
-  // useEffect(() => {
-  //   const printName = (i, word) => {
-  //     let text = "";
-  //     if (i > word.length - 1) {
-  //       return;
-  //     } else {
-  //       setTimeout(() => {
-  //         text = word[i];
-  //         nameRef.current.textContent += text;
-  //         i++;
-  //         printName(i, word);
-  //       }, 100);
-  //     }
-  //   };
-  //   printName(0, "Kator Agashua");
-  // }, []);
 
   return (
     <div className="home-page">
       <section className="hero grid xl:grid-cols-2 place-items-center gap-8 justify-between ">
         <div className="intro">
           <h2
-            className="greeting text-light-slate text-[2rem]"
+            className="greeting text-lighter-slate text-[2rem]"
             data-shadow={translations[current].greeting}
           >
             {translations[current].greeting} 👋🏾, {translations[current].art}{" "}
           </h2>
-          <h1
-            className="name font-extrabold xl:text-6xl text-[gold]"
-            ref={nameRef}
-          >
+          <h1 className="name font-extrabold xl:text-6xl text-lighter-slate">
             <TypewriterComponent />
           </h1>
           {}
-          <h2 className="intro-paragraph">
+          <h2 className="intro-paragraph text-light-slate">
             I am a Frontend Developer from Nigeria.{" "}
             <ReactCountryFlag
               countryCode="NG"
@@ -85,7 +62,7 @@ const Home = (props) => {
             me here.
           </h2>
           <h2 className="intro-paragraph">
-            For inquiries, or if you'd like to have a chat you can{" "}
+            If you'd like to have a chat you can{" "}
             <Link to="contact">
               <span className="home-links">CONTACT</span>
             </Link>{" "}
@@ -104,7 +81,7 @@ const Home = (props) => {
               d="M51.3,-72.9C64.9,-60.7,73.4,-43.6,77.7,-26.2C82,-8.8,82.2,9,75,22.3C67.8,35.5,53.2,44.3,39.5,52.7C25.7,61.1,12.9,69,-1.4,70.9C-15.7,72.8,-31.4,68.8,-46.5,60.9C-61.6,53,-76.1,41.2,-77.7,27.3C-79.3,13.5,-68,-2.5,-62.3,-20.3C-56.6,-38.1,-56.4,-57.8,-47,-71.3C-37.5,-84.9,-18.8,-92.2,0,-92.3C18.8,-92.3,37.7,-85,51.3,-72.9Z"
               transform="translate(100 100)"
             />
-  </svg>*/}
+  </svg> */}
 
           <img
             src="./images/peep-46.svg"
